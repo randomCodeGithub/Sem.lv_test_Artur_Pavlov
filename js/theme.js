91343f0089163e0432b0dc8e5e00be3ea9d400fb
Tawer <tawerwater70@gmail.com>
2020-08-03 17:52:25 +0300

    second

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,5 @@
 "use strict";
 $(document).ready(function () {
-
   if ($(window).width() > 960) {
     //iedvesmas stāsti
     for (let index = 21; index < 100; index++) {
@@ -27,23 +26,75 @@ $(document).ready(function () {
     $(this).remove();
   });
 
-  
-  let toggle = true;
-  //adding icon to toggler
+  // === SCROLLING ===
 
-  $(".navbar-toggler").click(function () {
-    if (toggle) {
-      $(".navbar-light .navbar-toggler-icon").css(
-        "background-image",
-        'url("../img/mobile/close-btn.svg")'
-      );
-      toggle = false;
-    } else {
-      $(".navbar-light .navbar-toggler-icon").css(
-        "background-image",
-        'url("../img/mobile/menu-toggler.svg")'
-      );
-      toggle = true;
+  let anchorLink = document.getElementById("anchor-link");
+
+  let timer = null;
+// IF NOT SCROLLING EVENT LISTENER
+window.addEventListener(
+  "scroll",
+  function () {
+    if (timer !== null) {
+      clearTimeout(timer);
     }
-  });
-});
+    timer = setTimeout(function () {
+      if (
+        !(window.innerHeight + window.scrollY >= document.body.offsetHeight)
+      ) {
+        anchorLink.style.display = "none";
+      } else {
+        anchorLink.style.display = "flex";
+      }
+    }, 1000);
+  },
+  false
+);
+
+// IF NOT SCROLLING EVENT LISTENER
+window.addEventListener(
+  "scroll",
+  function () {
+    anchorLink.style.display = "flex";
+    if (window.scrollY == 0) {
+      anchorLink.style.display = "none";
+    }
+  },
+  true
+);
+
+
+ });
+
+ //   let toggle = true;
+//   //adding icon to toggler
+
+//   $(".navbar-toggler").click(function () {
+//     if (toggle) {
+//       $(".navbar-light .navbar-toggler-icon").css(
+//         "background-image",
+//         'url("../img/mobile/close-btn.svg")'
+//       );
+//       toggle = false;
+//     } else {
+//       $(".navbar-light .navbar-toggler-icon").css(
+//         "background-image",
+//         'url("../img/mobile/menu-toggler.svg")'
+//       );
+//       toggle = true;
+//     }
+//   });
+
+//   let anchorLink = $("#anchor-link");
+//   let timer = null;
+
+//   $(window).scroll(function () {
+//     if (timer != null) {
+//       clearTimeout(timer);
+//     }
+
+//     timer = setTimeout(function() {
+//       if($(window).innerHeight() + $(window).scrollTop() >= )
+
+//     }, 1000);
+//   }, false);
